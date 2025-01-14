35aff3bde028dfe0012dc22ce3451f446a0a6796
Efim <efim0424@mail.ru>
2025-01-14 10:56:23 +0300

    feat: add fonts, configure Tailwind configs and SCSS

---
+++ src/constants/colors.constants.ts
@@ -0,0 +1,6 @@
+export const COLORS = {
+  primary: '#A42E35',
+  bg: '#1A1A28',
+  white: '#FEFEFD',
+  border: '#2E383E'
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import { COLORS } from './src/constants/colors.constants';
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: COLORS
+    },
+  },
+  plugins: [],
+} satisfies Config;
