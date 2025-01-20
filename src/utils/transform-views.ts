46370ed6ae49ea21c221d00ba3d2d361d7661f8b
Efim <efim0424@mail.ru>
2025-01-20 10:50:12 +0300

    feat: add UI component of the video block

---
+++ src/utils/transform-views.ts
@@ -0,0 +1,33 @@
+interface IViewsGradation {
+	value: number
+	designation: string
+}
+
+export function transformViews(views: number): string {
+	let formattedViews: string
+
+	const viewsGradation: IViewsGradation[] = [
+		{
+			value: 1_000_000_000,
+			designation: 'B'
+		},
+		{
+			value: 1_000_000,
+			designation: 'M'
+		},
+		{
+			value: 1_000,
+			designation: 'K'
+		}
+	]
+
+	for (const currentViews of viewsGradation) {
+		if (views >= currentViews.value) {
+			formattedViews = (views / currentViews.value).toFixed(1)
+			formattedViews = formattedViews.endsWith('.0') ? formattedViews.slice(0, -2) : formattedViews
+			return formattedViews
+		}
+	}
+
+	return `${views} views`
+}
