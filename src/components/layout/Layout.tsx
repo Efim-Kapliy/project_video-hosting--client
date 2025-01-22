41784d543bede414b80ad363ec70d38fbe153ec6
Efim <efim0424@mail.ru>
2025-01-22 20:40:24 +0300

    refactor: change how the sidebar hide animation works

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import cn from 'clsx'
-import { type PropsWithChildren, useEffect, useRef, useState } from 'react'
+import { type PropsWithChildren, useState } from 'react'
 
 import { Content } from './content/Content'
 import { Sidebar } from './sidebar/Sidebar'
@@ -10,15 +10,6 @@ import styles from './Layout.module.scss'
 
 export function Layout({ children }: PropsWithChildren<unknown>) {
 	const [isShowedSidebar, setIsShowedSidebar] = useState(true)
-	const firstRender = useRef(true)
-
-	useEffect(() => {
-		// TODO Можно в будущем вынести в store с сохранением состояния меню
-		if (firstRender.current) {
-			firstRender.current = false
-			return
-		}
-	})
 
 	const toggleSidebar = () => {
 		setIsShowedSidebar(!isShowedSidebar)
@@ -28,7 +19,8 @@ export function Layout({ children }: PropsWithChildren<unknown>) {
 		<main
 			className={cn(
 				'flex min-h-screen',
-				!firstRender.current && (isShowedSidebar ? styles.showedSidebar : styles.hidedSidebar)
+				styles.initialSidebar,
+				isShowedSidebar ? styles.showedSidebar : styles.hidedSidebar
 			)}
 		>
 			<Sidebar toggleSidebar={toggleSidebar} />
