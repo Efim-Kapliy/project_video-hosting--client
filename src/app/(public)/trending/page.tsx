e8d186790567bd62462a22dbd4910ff724f195d2
Efim <efim0424@mail.ru>
2025-01-24 15:53:50 +0300

    feat: add video to the trending block

--- src/app/(public)/trending/page.tsx
+++ src/app/(public)/trending/page.tsx
@@ -1,6 +1,15 @@
+import { Flame } from 'lucide-react'
 import type { Metadata } from 'next'
 
+import { Heading } from '@/ui/Heading'
+import { VideoItem } from '@/ui/video-item/VideoItem'
+
 import { PUBLIC_PAGE } from '@/config/public-page.config'
+
+import { videoService } from '@/services/video.service'
+
+export const revalidate = 100
+export const dynamic = 'force-static'
 
 export const metadata: Metadata = {
 	title: 'Trending',
@@ -15,6 +24,32 @@ export const metadata: Metadata = {
 	}
 }
 
-export default function TrendingPage() {
-	return <div>Trending</div>
+export default async function TrendingPage() {
+	const videos = await videoService.getTrendingVideos()
+
+	console.log(videos.data)
+
+	return (
+		<section>
+			<Heading
+				isH1
+				Icon={Flame}
+			>
+				Trending
+			</Heading>
+			<div className='grid-6-cols'>
+				{videos.data.length ? (
+					videos.data.map(video => (
+						<VideoItem
+							key={video.id}
+							video={video}
+							Icon={Flame}
+						/>
+					))
+				) : (
+					<div>Trends are temporarily unavailable</div>
+				)}
+			</div>
+		</section>
+	)
 }
