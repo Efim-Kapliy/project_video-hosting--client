9c346bc42c78ac317f233dd07575044ff9476fcd
Efim <efim0424@mail.ru>
2025-01-29 16:12:12 +0300

    feat: add notification, recaptcha and finalize authorization

---
+++ src/services/auth.service.ts
@@ -0,0 +1,82 @@
+import Cookies from 'js-cookie'
+
+import { axiosClassic } from '@/api/axios'
+
+import type { IAuthData } from '@/types/auth-form.types'
+import type { IUser } from '@/types/user.types'
+
+interface IAuthResponse {
+	user: IUser
+	accessToken: string
+}
+
+export enum EnumTokens {
+	'ACCESS_TOKEN' = 'accessToken',
+	'REFRESH_TOKEN' = 'refreshToken'
+}
+
+class AuthService {
+	private _BASE_URL = '/auth'
+
+	async main(type: 'login' | 'register', data: IAuthData, recaptchaToken?: string | null) {
+		const response = await axiosClassic.post<IAuthResponse>(`${this._BASE_URL}/${type}`, data, {
+			headers: {
+				recaptcha: recaptchaToken
+			}
+		})
+
+		if (response.data.accessToken) {
+			this._saveTokenStorage(response.data.accessToken)
+		}
+
+		return response
+	}
+
+	// CLIENT
+	async getNewTokens() {
+		const response = await axiosClassic.post<IAuthResponse>(`${this._BASE_URL}/access-token`)
+
+		if (response.data.accessToken) {
+			this._saveTokenStorage(response.data.accessToken)
+		}
+
+		return response
+	}
+
+	// SERVER
+	async getNewTokensByRefresh(refreshToken: string) {
+		const response = await axiosClassic.post<IAuthResponse>(
+			`${this._BASE_URL}/access-token`,
+			{},
+			{
+				headers: {
+					Cookie: `refreshToken=${refreshToken}`
+				}
+			}
+		)
+
+		return response.data
+	}
+
+	async logout() {
+		const response = await axiosClassic.post<boolean>(`${this._BASE_URL}/logout`)
+
+		if (response.data) this._removeFromStorage()
+
+		return response
+	}
+
+	private _saveTokenStorage(accessToken: string) {
+		Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
+			domain: 'localhost',
+			sameSite: 'strict',
+			expires: 1
+		})
+	}
+
+	private _removeFromStorage() {
+		Cookies.remove(EnumTokens.ACCESS_TOKEN)
+	}
+}
+
+export const authService = new AuthService()
